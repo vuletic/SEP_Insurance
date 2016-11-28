ea3ec6a165ccec56aa497eaae304671609801806
vuletic <vuletic93@gmail.com>
2016-11-28 19:02:27 +0100

    Testing github integration

--- SEP_Osiguranje/Controllers/HomeController.cs
+++ SEP_Osiguranje/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace SEP_Osiguranje.Controllers
              
                 Random rnd = new Random();
                 int r = rnd.Next(1000);
-                dbCtx.Vozilo.Add(new Vozilo() { Broj_registarske_tablice_Vozilo = "1234", Broj_sasije_Vozilo = "12121",  Godina_proizvodnje_Vozilo = 1982, Id_Osigurani_entitet = r, Ime_Vlasnik_Vozilo = "Djovak", JMBG_Vlasnik_Vozilo = "1231", Prezime_Vlasnik_Vozilo = "Njokovic" });
+                dbCtx.Vozilo.Add(new Vozilo() { Broj_registarske_tablice_Vozilo = "6666", Broj_sasije_Vozilo = "12121",  Godina_proizvodnje_Vozilo = 1982, Id_Osigurani_entitet = r, Ime_Vlasnik_Vozilo = "Djovak", JMBG_Vlasnik_Vozilo = "1231", Prezime_Vlasnik_Vozilo = "Njokovic" });
 
               
                 dbCtx.SaveChanges();
