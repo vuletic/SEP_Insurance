8a93cf52331cc2eaa267c2531bb39d2ed4eb2a51
vuletic <vuletic93@gmail.com>
2016-12-02 01:02:35 +0100

    Frontend prototype inserted

--- SEP_Osiguranje/Content/angular/app.route.js
+++ SEP_Osiguranje/Content/angular/app.route.js
@@ -5,7 +5,7 @@
 		.module('app')
         .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-      $urlRouterProvider.otherwise("/home");
+      $urlRouterProvider.otherwise("/process");
 
       $stateProvider
         .state('home', {
@@ -13,6 +13,12 @@
             templateUrl: 'Content/angular/app/components/core/home.html',
             controller: 'homeController',
             controllerAs: 'hc'
+        })
+        .state('process', {
+            url: '/process',
+            templateUrl: 'Content/angular/app/components/process/process.html',
+            controller: 'processController',
+            controllerAs: 'pc'
         })
         .state('prices', {
             url: '/prices',
