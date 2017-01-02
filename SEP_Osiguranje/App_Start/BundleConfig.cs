b8d122e66627c3fc8cbc5ca6ef88443f09edd6c2
nenadtod <nenadtod@live.com>
2017-01-02 16:55:00 +0100

    Default folder for bower components is now assets/components. Erased some redundant and unnecessary files.

--- SEP_Osiguranje/App_Start/BundleConfig.cs
+++ SEP_Osiguranje/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace SEP_Osiguranje
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -18,7 +18,7 @@ namespace SEP_Osiguranje
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+            */
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
