ee5aec5c994f5626f44155963092d8077a8b9226
nenadtod <nenadtod@live.com>
2017-01-02 19:16:24 +0100

    Initialized a controller for communication with RulesAPI.

---
+++ SEP_Osiguranje/Controllers/FinalizeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SEP_Osiguranje.Controllers
+{
+    public class FinalizeController : ApiController
+    {
+
+        static HttpClient client = new HttpClient();
+
+        public async Task Get()
+        {
+            HttpResponseMessage response = await client.GetAsync("http://sepruleapi.azurewebsites.net/rules");
+            if (response.IsSuccessStatusCode)
+            {
+                String a = await response.Content.ReadAsStringAsync();
+
+            }
+        }
+
+
+    }
+}
